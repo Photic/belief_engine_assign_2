ad0383a26584ea88ff62b53f62e9329cd66fc093
Steen Hansen <steen.hansen@ibm.com>
2021-05-14 18:01:49 +0200

    Done with adding simple sentence to beliefbase

---
+++ src/control/Pair.java
@@ -0,0 +1,6 @@
+package control;
+
+public class Pair<T1, T2> {
+    public T1 first;
+    public T2 last;
+}
--- src/control/UserInputControl.java
+++ src/control/UserInputControl.java
@@ -1,12 +1,17 @@
 package control;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 import model.BeliefBase;
 import model.Constants;
+import sentences.AndSentence;
 import sentences.AtomicSentence;
+import sentences.BiimplicationSentence;
+import sentences.ImplicationSentence;
+import sentences.NotSentence;
+import sentences.OrSentence;
+import sentences.Sentence;
 
 public class UserInputControl {
 
@@ -21,7 +26,7 @@ public class UserInputControl {
     }
 
     public void splitIntoBeliefBaseSentences() {
-        String input = "( alpha ,  alpha | !beta ,  !alpha & (beta | (!!gamma)) ,  alpha <=> (beta | gamma) | alpha => beta)";
+        String input = "( alpha ,  alpha | !beta ,  !alpha & (beta | (!!gamma)) , alpha <=> ((beta | gamma)), !beta )";
         System.out.println(input);
         input = removeLeadingAndTrailingEncapsulations(input);
         String[] split = input.split(",");
@@ -48,6 +53,7 @@ public class UserInputControl {
 
         findMoreComplexSentences(splitArray, beliefBase);
 
+        System.out.println(beliefBase);
     }
 
     public void findMoreComplexSentences(List<String> split, BeliefBase beliefBase) {
@@ -64,14 +70,105 @@ public class UserInputControl {
         System.out.println(resolvedSentences);
 
         for (List<String> list : resolvedSentences) {
-            addResolvedSentencesToBeliefBase(list);
+            addResolvedSentencesToBeliefBase(list, beliefBase);
         }
     }
-    
-        
-    private void addResolvedSentencesToBeliefBase(List<String> resolvedSentence) {
-        
 
+    // ( alpha , alpha | !beta , !alpha & (beta | (!!gamma)) , alpha <=> (beta |
+    // gamma) )
+
+    /*
+     * bBase.add(alpha);
+     * 
+     * bBase.add(new OrSentence(new AtomicSentence(alpha), new
+     * AtomicSentence(notBeta)));
+     * 
+     * bBase.add(new AndSentence(new AtomicSentence(notAlpha), new OrSentence(new
+     * AtomicSentence(beta), new NotSentence(new AtomicSentence(notGamma)))));
+     * 
+     * bBase.add(new BiimplicationSentence( new AtomicSentence(alpha), new
+     * OrSentence( new AtomicSentence(beta), new AtomicSentence(gamma) )));
+     */
+
+    private void addResolvedSentencesToBeliefBase(List<String> resolvedSentence, BeliefBase beliefBase) {
+        List<Class> functions = new ArrayList<>();
+
+        for (String string : resolvedSentence) {
+            if (Pattern.matches("^.*[\\(\\)].*$", string)) {
+                // TODO, more complex sentence
+                return;
+            }
+        }
+
+        simpleBeliefBaseAddition(resolvedSentence, beliefBase);
+    }
+
+    private void complexBeliefBaseAddition(List<String> sentence, BeliefBase beliefBase) {
+
+    }
+
+    private void simpleBeliefBaseAddition(List<String> sentence, BeliefBase beliefBase) {
+        boolean isAConstant = false;
+        String pairingSentence = "";
+        Pair<Sentence, Sentence> pairs = new Pair<>();
+
+        for (String string : sentence) {
+            for (String con : constants) {
+                if (string.contains(con)) {
+                    pairingSentence = con;
+                    isAConstant = true;
+                    break;
+                }
+            }
+            if (!isAConstant) {
+                if (pairs.first == null) {
+                    pairs.first = literalResolution(string);
+                } else {
+                    pairs.last = literalResolution(string);
+                }
+            }
+            isAConstant = false;
+        }
+
+        beliefBase.add(pairingSentenceResolved(pairingSentence, pairs));
+    }
+
+    private Sentence literalResolution(String literal) {
+        int nots = 0;
+
+        for (int i = 0; i < literal.length(); i++) {
+            if (literal.charAt(i) == Constants.NOT.charAt(0)) {
+                nots++;
+            }
+        }
+
+        if (nots == 0) {
+            return new AtomicSentence(literal);
+        }
+
+        if (nots == 1) {
+            return new NotSentence(new AtomicSentence(literal.replaceAll("\\!", "")));
+        }
+
+        if (nots == 2) {
+            return new NotSentence(new NotSentence(new AtomicSentence(literal.replaceAll("\\!", ""))));
+        }
+
+        return null;
+    }
+
+    private Sentence pairingSentenceResolved(String con, Pair<Sentence, Sentence> sentences) {
+        if (Constants.OR.equals(con)) {
+            return new OrSentence(sentences.first, sentences.last);
+        } else if (Constants.AND.equals(con)) {
+            return new AndSentence(sentences.first, sentences.last);
+        } else if (Constants.IFF.equals(con)) {
+            return new BiimplicationSentence(sentences.first, sentences.last);
+        } else if (Constants.IF.equals(con)) {
+            return new ImplicationSentence(sentences.first, sentences.last);
+        }
+
+        return null;
     }
 
     private List<String> handleInputOutputList(List<String> input) {
@@ -102,7 +199,7 @@ public class UserInputControl {
         for (int i = 0; i < output.size(); i++) {
             for (String cos : constants) {
                 if (output.get(i).length() > cos.length() + 1 && output.get(i).contains(cos)) {
-                    return handleInputOutputList(output); 
+                    return handleInputOutputList(output);
                 }
             }
         }
@@ -139,8 +236,6 @@ public class UserInputControl {
         return output;
     }
 
-    // TODO There might be a problem if a single sentences contains not, test and
-    // fix later.
     public void findSingularAtomicSentences(List<String> split, BeliefBase beliefBase) {
         boolean add = true;
 
@@ -153,10 +248,10 @@ public class UserInputControl {
                 }
             }
             if (add) {
-                beliefBase.add(new AtomicSentence(string));
-                removeObj.add(string);
-                add = true;
+                beliefBase.add(literalResolution(string));
+                removeObj.add(string.replaceAll("\\s+", ""));
             }
+            add = true;
         }
 
         for (String string : removeObj) {
@@ -165,19 +260,10 @@ public class UserInputControl {
     }
 
     public String removeLeadingAndTrailingEncapsulations(String input) {
-        return input.substring(1, input.length() - 1);
+        if (input.charAt(0) == '(' && input.charAt(input.length() - 1) == ')') {
+            return input.substring(1, input.length() - 1);
+        }
+        return input;
     }
 
 }
-
-// ( alpha , alpha | !beta , !alpha & (beta | (!!gamma)) , alpha <=> (beta |
-// gamma) )
-
-/*
- * bBase.add(alpha); bBase.add(new OrSentence(new AtomicSentence(alpha), new
- * AtomicSentence(notBeta))); bBase.add(new AndSentence(new
- * AtomicSentence(notAlpha), new OrSentence(new AtomicSentence(beta), new
- * NotSentence(new AtomicSentence(notGamma))))); bBase.add(new
- * BiimplicationSentence( new AtomicSentence(alpha), new OrSentence( new
- * AtomicSentence(beta), new AtomicSentence(gamma) ) ));
- */
