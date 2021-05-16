0bb33e3d0a9bb07bae72d635d5d82515fa4b6a77
Steen Hansen <steen.hansen@ibm.com>
2021-05-17 01:03:50 +0200

    Update RunMe.java

--- src/RunMe.java
+++ src/RunMe.java
@@ -149,12 +149,15 @@ public class RunMe {
 
 				switch (line.toLowerCase()) {
 					case "1":
+						System.out.println();
 						revisionPostulateTests();
 						break;
 					case "2":
+						System.out.println();
 						contractionPostulateTests();
 						break;
 					case "3":
+						System.out.println();
 						SentenceToCNFTest();
 						break;
 					case "b":
@@ -195,11 +198,11 @@ public class RunMe {
     					)
 		);
 				
-    	System.out.println(String.format("Sentence before CNF conversion: %s", distriTest.toString()));
+    	System.out.println(String.format("\t | Sentence before CNF conversion: %s", distriTest.toString()));
     	distriTest = distriTest.convertToCNF();
-    	System.out.println(String.format("Sentence after CNF conversion: %s", distriTest.toString()));
-    	System.out.println(String.format("Sentence isInCNF()-evaluation: %s", distriTest.isInCNF()));
-    	System.out.println(String.format("distriTest predicate list: %s", distriTest.getPredicates()));
+    	System.out.println(String.format("\t | Sentence after CNF conversion: %s", distriTest.toString()));
+    	System.out.println(String.format("\t | Sentence isInCNF()-evaluation: %s", distriTest.isInCNF()));
+    	System.out.println(String.format("\t | distriTest predicate list: %s", distriTest.getPredicates()));
     }
     
 	public static void revisionPostulateTests() {
@@ -217,45 +220,45 @@ public class RunMe {
     }
 
 	private static void contractionSuccess() {
-		System.out.println("Testing the Success postulate for contraction");
+		System.out.println("\t | Testing the Success postulate for contraction");
 		BeliefBase bBase = new BeliefBase();
     	bBase.expand(new AtomicSentence("p"));
     	bBase.expand(new ImplicationSentence(new AtomicSentence("p"), new AtomicSentence("q")));
-		System.out.println(String.format("Elements in the belief base: %s", bBase));
+		System.out.println(String.format("\t | Elements in the belief base: %s", bBase));
 		bBase.contract(new ImplicationSentence(new AtomicSentence("p"), new AtomicSentence("q")));
-		System.out.println(String.format("Elements in the belief base after contraction with q: %s", bBase));
-		System.out.println("The implication was no longer valid, and thus is it no longer in the resulting belief set");
+		System.out.println(String.format("\t | Elements in the belief base after contraction with q: %s", bBase));
+		System.out.println("\t | The implication was no longer valid, and thus is it no longer in the resulting belief set");
 		System.out.println();
 	}
 
 	private static void contractionInclusion() {
-		System.out.println("Testing the Inclusion postulate for contraction");
+		System.out.println("\t | Testing the Inclusion postulate for contraction");
 		BeliefBase bBase = new BeliefBase();
     	bBase.expand(new AtomicSentence("p"));
     	bBase.expand(new AtomicSentence("q"));
     	bBase.expand(new AtomicSentence("r"));
-		System.out.println(String.format("Elements in the belief base: %s", bBase));
+		System.out.println(String.format("\t | Elements in the belief base: %s", bBase));
 		bBase.contract(new AtomicSentence("r"));
-		System.out.println(String.format("Elements in the belief base after contraction with r: %s", bBase));
-		System.out.println("We can see that the belief set after contraction with r is a subset of the original belief set");
+		System.out.println(String.format("\t | Elements in the belief base after contraction with r: %s", bBase));
+		System.out.println("\t | We can see that the belief set after contraction with r is a subset of the original belief set");
 		System.out.println();
 	}
 
 	private static void contractionVacuity() {
-		System.out.println("Testing the Vacuity postulate for contraction");
+		System.out.println("\t | Testing the Vacuity postulate for contraction");
 		BeliefBase bBase = new BeliefBase();
     	bBase.expand(new AtomicSentence("p"));
     	bBase.expand(new AtomicSentence("q"));
     	bBase.expand(new AtomicSentence("r"));
-		System.out.println(String.format("Elements in the belief base: %s", bBase));
+		System.out.println(String.format("\t | Elements in the belief base: %s", bBase));
 		bBase.contract(new AtomicSentence("s"));
-		System.out.println(String.format("Elements in the belief base after contraction with s: %s", bBase));
-		System.out.println("Since s wasn't in the belief base, the contraction does not change the belief set");
+		System.out.println(String.format("\t | Elements in the belief base after contraction with s: %s", bBase));
+		System.out.println("\t | Since s wasn't in the belief base, the contraction does not change the belief set");
 		System.out.println();
 	}
 
 	private static void contractionExtensionality() {
-		System.out.println("Testing the Extensionality postulate for contraction");
+		System.out.println("\t | Testing the Extensionality postulate for contraction");
 		BeliefBase bBase1 = new BeliefBase();
 		bBase1.expand(new ImplicationSentence(new AtomicSentence("p"), new AtomicSentence("q")));
 		bBase1.expand(new AtomicSentence("p"));
@@ -266,28 +269,28 @@ public class RunMe {
     	bBase2.expand(new AtomicSentence("p"));
 		bBase2.expand(new AtomicSentence("q"));
 		bBase2.expand(new AtomicSentence("r"));
-		System.out.println(String.format("Elements in the first belief base: %s", bBase1));
-		System.out.println(String.format("Elements in the second belief base: %s", bBase2));
+		System.out.println(String.format("\t | Elements in the first belief base: %s", bBase1));
+		System.out.println(String.format("\t | Elements in the second belief base: %s", bBase2));
 		bBase1.contract(new OrSentence(new NotSentence(new AtomicSentence("p")), new AtomicSentence("q")));
 		bBase2.contract(new ImplicationSentence(new AtomicSentence("p"), new AtomicSentence("q")));
-		System.out.println(String.format("Elements in the first belief base after revision with (!p | q): %s", bBase1));
-		System.out.println(String.format("Elements in the second belief base after revision with (p => q): %s", bBase2));
-		System.out.println("We can see that the contractions behave in the same way, and the extensionality postulate is fulfilled.");
+		System.out.println(String.format("\t | Elements in the first belief base after revision with (!p | q): %s", bBase1));
+		System.out.println(String.format("\t | Elements in the second belief base after revision with (p => q): %s", bBase2));
+		System.out.println("\t | We can see that the contractions behave in the same way, and the extensionality postulate is fulfilled.");
 		System.out.println();
 	}
 
 	private static void revisionSuccess() {
-		System.out.println("Testing the Success postulate for revision");
+		System.out.println("\t | Testing the Success postulate for revision");
 		BeliefBase bBase = new BeliefBase();
-		System.out.println(String.format("Elements in the belief base: %s", bBase));
+		System.out.println(String.format("\t | Elements in the belief base: %s", bBase));
     	bBase.revise(new AtomicSentence("p"));
-		System.out.println(String.format("Elements in the belief base after revision with p: %s", bBase));
-		System.out.println(String.format("Does the belief base contain p?: %s", bBase.contains(new AtomicSentence("p"))));
+		System.out.println(String.format("\t | Elements in the belief base after revision with p: %s", bBase));
+		System.out.println(String.format("\t | Does the belief base contain p?: %s", bBase.contains(new AtomicSentence("p"))));
 		System.out.println();
 	}
 
 	private static void revisionInclusion() {
-		System.out.println("Testing the Inclusion postulate for revision");
+		System.out.println("\t | Testing the Inclusion postulate for revision");
 		BeliefBase bBase1 = new BeliefBase();
     	bBase1.expand(new AtomicSentence("p"));
     	bBase1.expand(new AtomicSentence("q"));
@@ -296,44 +299,44 @@ public class RunMe {
     	bBase2.expand(new AtomicSentence("p"));
     	bBase2.expand(new AtomicSentence("q"));
     	bBase2.expand(new AtomicSentence("r"));
-		System.out.println(String.format("Elements in the first belief base: %s", bBase1));
-		System.out.println(String.format("Elements in the second belief base: %s", bBase2));
+		System.out.println(String.format("\t | Elements in the first belief base: %s", bBase1));
+		System.out.println(String.format("\t | Elements in the second belief base: %s", bBase2));
 		bBase1.revise(new AtomicSentence("s"));
-		System.out.println(String.format("Elements in the first belief base after revision with s: %s", bBase1));
+		System.out.println(String.format("\t | Elements in the first belief base after revision with s: %s", bBase1));
 		bBase2.expand(new AtomicSentence("s"));
-		System.out.println(String.format("Elements in the second belief base after expanding with s: %s", bBase2));
-		System.out.println("We can see they contain the same elements, therefore the first belief base is a subset of the second.");
+		System.out.println(String.format("\t | Elements in the second belief base after expanding with s: %s", bBase2));
+		System.out.println("\t | We can see they contain the same elements, therefore the first belief base is a subset of the second.");
 		System.out.println();
 	}
 
 	private static void revisionVacuity() {
-		System.out.println("Testing the Vacuity postulate for revision");
+		System.out.println("\t | Testing the Vacuity postulate for revision");
 		BeliefBase bBase = new BeliefBase();
     	bBase.expand(new AtomicSentence("p"));
     	bBase.expand(new AtomicSentence("q"));
     	bBase.expand(new AtomicSentence("r"));
-		System.out.println(String.format("Elements in the belief base: %s", bBase));
+		System.out.println(String.format("\t | Elements in the belief base: %s", bBase));
 		bBase.revise(new AtomicSentence("s"));
-		System.out.println(String.format("Elements in the belief base after revision with s: %s", bBase));
-		System.out.println("Since !s wasn't in the belief base, the revision behaves as expand");
+		System.out.println(String.format("\t | Elements in the belief base after revision with s: %s", bBase));
+		System.out.println("\t | Since !s wasn't in the belief base, the revision behaves as expand");
 		System.out.println();
 	}
 
 	private static void revisionExtensionality() {
-		System.out.println("Testing the Extensionality postulate for revision");
+		System.out.println("\t | Testing the Extensionality postulate for revision");
 		BeliefBase bBase1 = new BeliefBase();
     	bBase1.expand(new AtomicSentence("p"));
     	bBase1.expand(new AtomicSentence("q"));
     	BeliefBase bBase2 = new BeliefBase();
     	bBase2.expand(new AtomicSentence("p"));
     	bBase2.expand(new AtomicSentence("q"));
-		System.out.println(String.format("Elements in the first belief base: %s", bBase1));
-		System.out.println(String.format("Elements in the second belief base: %s", bBase2));
+		System.out.println(String.format("\t | Elements in the first belief base: %s", bBase1));
+		System.out.println(String.format("\t | Elements in the second belief base: %s", bBase2));
 		bBase1.revise(new OrSentence(new NotSentence(new AtomicSentence("p")), new AtomicSentence("q")));
-		System.out.println(String.format("Elements in the first belief base after revision with (!p | q): %s", bBase1));
+		System.out.println(String.format("\t | Elements in the first belief base after revision with (!p | q): %s", bBase1));
 		bBase2.revise(new ImplicationSentence(new AtomicSentence("p"), new AtomicSentence("q")));
-		System.out.println(String.format("Elements in the second belief base after revision with (p => q): %s", bBase2));
-		System.out.println("We can see that the revisions behave in the same way, and the extensionality postulate is fulfilled.");
+		System.out.println(String.format("\t | Elements in the second belief base after revision with (p => q): %s", bBase2));
+		System.out.println("\t | We can see that the revisions behave in the same way, and the extensionality postulate is fulfilled.");
 		System.out.println();
 	}
 }
